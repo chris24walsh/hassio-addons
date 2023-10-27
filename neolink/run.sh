ad4db70340d4840b53d9ed10b0932c86cfde561a
Chris Walsh <chris24walsh@gmail.com>
2023-10-27 10:59:53 +0100

    Update run.sh

--- neolink/run.sh
+++ neolink/run.sh
@@ -7,7 +7,7 @@ CONFIG_PATH=/data/options.json
 MODE=$(jq --raw-output '.mode // empty' $CONFIG_PATH)
 
 echo "--- VERSIONS ---"
-echo "add-on version: 0.0.7.1"
+echo "add-on version: 0.0.7.2"
 echo -n "neolink version: " && neolink --version
 echo "neolink mode: ${MODE}"
 echo "ATTENTION: if you expected a newer Neolink version, please reinstall this Add-on!"
