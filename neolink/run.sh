250a47f7436231000188108246b824b271da32f8
Chris Walsh <christopher.walsh@leaseplan.com>
2023-10-27 11:08:06 +0100

    Add debug env var

--- neolink/run.sh
+++ neolink/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-RUST_LOG="neolink=debug"
+ENV RUST_LOG debug
+
 CONFIG_PATH=/data/options.json
 
 MODE=$(jq --raw-output '.mode // empty' $CONFIG_PATH)
 
 echo "--- VERSIONS ---"
-echo "add-on version: 0.0.7.2"
+echo "add-on version: 0.0.7.3"
 echo -n "neolink version: " && neolink --version
 echo "neolink mode: ${MODE}"
 echo "ATTENTION: if you expected a newer Neolink version, please reinstall this Add-on!"
